8b2f198bdd8ce51e47546a64a3446a71269b5b96
Adeesh-bode <bodead@rknec.edu>
2024-11-20 19:17:18 +0530

    auth module & user service

---
+++ src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+    username: string;
+    email: string;
+    password: string;
+    userId: number;
+};
+
+// FIXME: implement actual db 
+const users: User[]=[
+    {
+        username: 'john',
+        email: 'john@gmail.com',
+        password: 'changeme',
+        userId: 1
+    },
+    {
+        username: 'chris',
+        email: 'chris@gmail.com',
+        password: 'secret',
+        userId: 2
+    }
+]
+
+@Injectable()
+export class UsersService {
+    async findUserByUsername(username: string): Promise<User | undefined> {
+        // FIXME:prisma query
+        return users.find(user=>user.username===username);
+        
+    }
+}
